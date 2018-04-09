e8a294e549475653c8507cd6864b1b9fa23874a8
Jerrad Christian <jerradchristian@yahoo.com>
2018-04-09 15:50:56 -0400

    shared layout / deck view / deck controller

---
+++ Capstone.Web/Controllers/DeckController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class DeckController : Controller
+    {
+        // GET: Deck
+        public ActionResult Deck()
+        {
+            return View("DeckView");
+        }
+    }
+}
