341f3a954039db3af852337282eadd34c8713a53
Jerrad Christian <jerradchristian@yahoo.com>
2018-04-10 11:10:02 -0400

    login controller/view, still nedds verification

--- Capstone.Web/Controllers/DeckController.cs
+++ Capstone.Web/Controllers/DeckController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//using Capstone.Web.DAL;
+using Capstone.Web.DAL;
 using System.Configuration;
 
 namespace Capstone.Web.Controllers
