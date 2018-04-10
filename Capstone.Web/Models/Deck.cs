6a7c775a087f3edbc80660c7ceedc1839bf446ad
Jason Norris <norrisjas@hotmail.com>
2018-04-10 11:20:58 -0400

    add Deck DAL, Model and initial Test

---
+++ Capstone.Web/Models/Deck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class Deck
+    {
+        public int DeckID { get; set; }
+
+        public int UserID { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsPublic { get; set; }
+
+    }
+}
