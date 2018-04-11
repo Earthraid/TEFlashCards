34ddd1ab04345feab7352bb0443e059817afed83
Jason Norris <norrisjas@hotmail.com>
2018-04-11 19:07:01 -0400

    substantial Tag SQL for adding to Card and Deck with integration in Models

--- Capstone.Web/Models/Card.cs
+++ Capstone.Web/Models/Card.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Capstone.Web.DAL;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +9,45 @@ namespace Capstone.Web.Models
 {
     public class Card
     {
+        private string connectionString = ConfigurationManager.ConnectionStrings["HotelFlashCardsDB"].ConnectionString;
+
         public string CardID { get; set; }
         public string Front { get; set; }
         public string Back { get; set; }
         public string UserID { get; set; }
+
+        /// <summary>
+        /// Returns a list of all available Tags.
+        /// </summary>
+        public List<string> AllTags
+        {
+            get
+            {
+                TagsSqlDAL tagSql = new TagsSqlDAL(connectionString);
+                return tagSql.TagList;
+            }
+        }
+
+        /// <summary>
+        /// Returns the Tags associated with an individual card in the current instance.
+        /// </summary>
+        public List<string> ThisCardTags
+        {
+            get
+            {
+                TagsSqlDAL tagSql = new TagsSqlDAL(connectionString);
+                return tagSql.GetTagsByCardID(CardID);
+            }
+        }
+
+        /// <summary>
+        /// Adds a Tag to an individual card in a current instance.
+        /// </summary>
+        /// <param name="tagName"></param>
+        public void AddTagToCard(string tagName)
+        {
+            TagsSqlDAL tagsSql = new TagsSqlDAL(connectionString);
+            tagsSql.AddTagToCard(CardID, tagName);
+        }
     }
 }
--- Capstone.Web/Models/Deck.cs
+++ Capstone.Web/Models/Deck.cs
@@ -20,7 +20,6 @@ namespace Capstone.Web.Models
 
         public bool IsPublic { get; set; }
 
-
         public List<Card> DeckCards(string deck_id)
         {
             CardSqlDAL c = new CardSqlDAL(connectionString);
@@ -28,6 +27,40 @@ namespace Capstone.Web.Models
 
             return cards;
 
+        }
+        
+        /// <summary>
+        /// Returns a list of all available Tags.
+        /// </summary>
+        public List<string> AllTags
+        {
+            get
+            {
+                TagsSqlDAL tagSql = new TagsSqlDAL(connectionString);
+                return tagSql.TagList;
+            }
+        }
+
+        /// <summary>
+        /// Returns the Tags associated with an individual deck in the current instance.
+        /// </summary>
+        public List<string> ThisDeckTags
+        {
+            get
+            {
+                TagsSqlDAL tagSql = new TagsSqlDAL(connectionString);
+                return tagSql.GetTagsByDeckID(DeckID);
+            }
+        }
+        
+        /// <summary>
+        /// Adds a Tag to an individual deck in a current instance.
+        /// </summary>
+        /// <param name="tagName"></param>
+        public void AddTagToDeck(string tagName)
+        {
+            TagsSqlDAL tagsSql = new TagsSqlDAL(connectionString);
+            tagsSql.AddTagToDeck(DeckID, tagName);
         }
     }
 }
